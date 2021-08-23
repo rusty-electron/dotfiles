bfb6ba6f33fd0895e7e05abb4db200ecabde5b1a
rusty-electron <pritom.gogoi101@gmail.com>
2021-08-23 17:51:05 +0530

    add emacs config and regular updates

---
+++ .emacs.d/init.el
@@ -0,0 +1,60 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(unless (package-installed-p 'spacemacs-theme)
+  (package-refresh-contents)
+  (package-install 'spacemacs-theme))
+
+;; org file start
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(org-babel-load-file (expand-file-name "~/.emacs.d/config.org")) 
+
+;; (global-set-key (kbd "<s-return>") 'ansi-term)
+
+(setq scroll-conservatively 100)
+(setq ring-bell-function 'ignore)
+(when window-system (global-hl-line-mode t))
+(when window-system (global-prettify-symbols-mode t))
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+;;(use-package beacon
+;;  :ensure t
+;;  :init
+;;  (beacon-mode 1))
+
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+(scroll-bar-mode -1)
+
+(setq inhibit-startup-message t)
+
+;; org file end
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(spacemacs-dark))
+ '(custom-safe-themes
+   '("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default))
+ '(package-selected-packages
+   '(switch-window rainbow-mode avy smex ido-vertical-mode org-bullets beacon spacemacs-dark spacemacs-theme which-key use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "ADBO" :family "Iosevka")))))
